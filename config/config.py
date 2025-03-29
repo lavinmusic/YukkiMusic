ab0201fdd69285408b1b1649fd7db2f613920647
lavinmusic <laaviinn17@gmail.com>
2025-03-29 13:04:39 +0300

    Update config.py
    Signed-off-by: lavinmusic <laaviinn17@gmail.com>

--- config/config.py
+++ config/config.py
@@ -93,11 +93,11 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheTeamVivek"
-)  # Example:- https://t.me/TheTeamVivek
+    "SUPPORT_CHANNEL", "https://t.me/musiclavu"
+)  # Example:- https://t.me/musiclavu
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TheTeamVk"
-)  # Example:- https://t.me/TheTeamVk
+    "SUPPORT_GROUP", "https://t.me/musiclavu"
+)  # Example:- https://t.me/musiclavu
 
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
