7a0772d64177bdb2906f77fe6e8c92e4356ac951
lavinmusic <laaviinn17@gmail.com>
2025-03-31 16:58:16 +0300

    Update call.py
    Signed-off-by: lavinmusic <laaviinn17@gmail.com>

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -3,7 +3,7 @@
 #
 # This file is part of < https://github.com/lavinmusic/YukkiMusic/ > project,
 # and is released under the MIT License.
-# Please see < https://github.com/lavinmusic/YukkiMusic//blob/master/LICENSE >
+# Please see < https://github.com/lavinmusic/YukkiMusic/blob/master/LICENSE >
 #
 # All rights reserved.
 #
